139c15f8751ab8245eba2c4c76d1d7cc252052f3
yenhi26052005-hue <yenhi26052005@gmail.com>
2026-03-18 16:09:22 +0700

    bai 7.3

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,16 @@ Route::get('/Huong', function () {
 Route::get('/legialoc', function () {
     return "LeGiaLoc";
 });
+
+use Illuminate\Support\Facades\DB;
+
+Route::get('/top-budget', function () {
+
+    $movies = DB::table('movie')
+        ->orderBy('budget', 'desc')
+        ->limit(10)
+        ->get();
+
+    return view('top_budget', compact('movies'));
+
+});
