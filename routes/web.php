9a0382e49bd83bfa4e29882d0385775d849d20a2
yenhi26052005-hue <yenhi26052005@gmail.com>
2026-03-25 23:01:01 +0700

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,53 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MovieController;
-use Illuminate\Support\Facades\DB;
-use App\Http\Controllers\BookController_3;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Route for TestController@test
-Route::get('/test', [\App\Http\Controllers\TestController::class, 'test']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/Nhi', function () {
-    return "NguyenThiYenNhi";
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/lenhi', function () {
-    return 'NguyenLeYenNhi';
-});
-
-Route::get('/Huong', function () {
-    return "BuiThiThuHuong";
-});
-
-Route::get('/legialoc', function () {
-    return "LeGiaLoc";
-});
-
-//7.1
-Route::get('/theloai', '\App\Http\Controllers\Movie@theloai');
-
-//7.2
-Route::get('/top-movies', [MovieController::class, 'topMovies']);
-
-//7.4
-Route::get('/phim/thoi-luong', [MovieController::class, 'longRuntime']);
-
-// top budget
-Route::get('/top-budget', function () {
-
-    $movies = DB::table('movie')
-        ->orderBy('budget', 'desc')
-        ->limit(10)
-        ->get();
-
-    return view('top_budget', compact('movies'));
-});
-//nhi
-use App\Http\Controllers\BookController;
-
-Route::get('/book/{id}', [BookController::class, 'show']);
+require __DIR__.'/auth.php';
