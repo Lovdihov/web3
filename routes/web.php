e64eaddee7d1f716f111679def2e08e5558a3617
btThuHuong <030239230066@st.buh.edu.vn>
2026-03-26 01:20:29 +0700

    Hoàn thành Xây dựng Layout

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieController;
+use App\Http\Controllers\BookController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -27,3 +28,8 @@ Route::get('/legialoc', function () {
 
 //7.2
 Route::get('/top-movies', [MovieController::class, 'topMovies']);
+
+
+
+//Layout
+Route::get('/sach', [BookController::class, 'sach']);
